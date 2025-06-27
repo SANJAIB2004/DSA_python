8407acdade3f060c7b3e2e47193c302f2f039254
SANJAIB2004 <sanjai.b2022cse@sece.ac.in>
2025-06-27 18:57:32 +0530

    newly tried pattern

--- PatternPrinting.py
+++ PatternPrinting.py
@@ -269,6 +269,36 @@ n = 5
     # 4     2 2
     # 5 4 3 2 1
 
+# ----------------------------------------------------------------------------------#
+
+s = "helloworld"
+res = [['0' for _ in range(len(s))]for _ in range(len(s))]
+print(*res)
+# Initialize a list of the same length as s
+for i in range(len(s)):
+    for j in range(len(s)):
+        if i==j:
+            res[i][j] = s[i]
+        if i+j == len(s)-1:
+            res[i][j] = s[i]
+
+for row in res:
+    for val in row:
+        print(f"{val if val != '0' else ' '}", end=" ")
+    print()
+
+# output:
+# h                 h
+#   e             e
+#     l         l
+#       l     l
+#         o o
+#         w w
+#       o     o
+#     r         r
+#   l             l
+# d                 d
+
 
 
 
